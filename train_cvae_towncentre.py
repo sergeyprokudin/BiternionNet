e0664fa894d6aff43ef22a72519f14151a247d9e
Sergey Prokudin <sergey.prokudin@gmail.com>
2017-07-18 21:16:35 +0200

    kl divergence annealing

--- train_cvae_towncentre.py
+++ train_cvae_towncentre.py
@@ -58,9 +58,9 @@ def main():
                           image_width=image_width,
                           n_channels=n_channels,
                           n_hidden_units=n_u,
-                          kl_weight=0.5)
+                          kl_weight=0.7)
 
-        cvae_model.full_model.fit([xtr, ytr_bit], [ytr_bit], batch_size=10, epochs=20,
+        cvae_model.full_model.fit([xtr, ytr_bit], [ytr_bit], batch_size=10, epochs=50,
                                   validation_data=([xval, yval_bit], yval_bit),
                                   callbacks=[tensorboard_callback, csv_callback, model_ckpt_callback])
 
