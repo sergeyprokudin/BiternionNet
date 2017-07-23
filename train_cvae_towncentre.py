69af01bd4636d5b52e2f3408326adda129bb3455
Sergey Prokudin <sergey.prokudin@gmail.com>
2017-07-23 13:49:49 +0200

    training without prior

--- train_cvae_towncentre.py
+++ train_cvae_towncentre.py
@@ -61,7 +61,7 @@ def main():
                           n_hidden_units=n_u,
                           kl_weight=1.0)
 
-        cvae_model.full_model.fit([xtr, ytr_bit], [ytr_bit], batch_size=100, epochs=n_epochs,
+        cvae_model.full_model.fit([xtr, ytr_bit], [ytr_bit], batch_size=10, epochs=n_epochs,
                                   validation_data=([xval, yval_bit], yval_bit),
                                   callbacks=[tensorboard_callback, csv_callback, model_ckpt_callback])
 
