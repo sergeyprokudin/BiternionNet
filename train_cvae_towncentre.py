9a9aa66bc73772b618232ccf3a445baf14abb63f
sergeyprokudin <sergey.prokudin@gmail.com>
2017-07-25 12:42:24 +0200

    kl 0.5->1.0, 5 epochs

--- train_cvae_towncentre.py
+++ train_cvae_towncentre.py
@@ -61,7 +61,7 @@ def main():
                           image_width=image_width,
                           n_channels=n_channels,
                           n_hidden_units=n_u,
-                          kl_weight=0.7)
+                          kl_weight=0.5)
 
         cvae_model.full_model.fit([xtr, ytr_bit], [ytr_bit], batch_size=batch_size, epochs=n_epochs,
                                   validation_data=([xval, yval_bit], yval_bit),
@@ -71,7 +71,7 @@ def main():
         cvae_model.evaluate(xval, yval_deg, 'validation')
         cvae_model.evaluate(xte, yte_deg, 'test')
 
-        kl_weight_range = [0.8, 0.9, 1.0]
+        kl_weight_range = [0.6, 0.7, 0.8, 0.9, 1.0]
 
         for kl_weight in kl_weight_range:
 
