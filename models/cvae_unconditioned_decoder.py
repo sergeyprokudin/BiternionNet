239bd73707d54be5e02a84e3cf4f23c0a7130fbb
sergeyprokudin <sergey.prokudin@gmail.com>
2017-08-08 13:18:21 +0200

    temp

--- models/cvae_unconditioned_decoder.py
+++ models/cvae_unconditioned_decoder.py
@@ -122,7 +122,7 @@ class CVAE:
         kappa_pred = model_output[:, self.n_u*5+2:]
         reconstruction_err = von_mises_log_likelihood_tf(y_true, mu_pred, kappa_pred)
         # kl = K.sum(K.abs(mu_prior-mu_encoder))
-        # kl = gaussian_kl_divergence_tf(mu_encoder, log_var_encoder, mu_prior, log_var_prior)
+        kl = gaussian_kl_divergence_tf(mu_encoder, log_var_encoder, mu_prior, log_var_prior)
         elbo = reconstruction_err - self.kl_weight*kl
         return K.mean(-elbo)
 
