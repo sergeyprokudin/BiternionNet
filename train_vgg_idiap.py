3b20442d19603f1453c82f5f24a33bee56cbfecb
sergeyprokudin <sergey.prokudin@gmail.com>
2017-09-21 18:30:27 +0200

    IDIAP dataset training added

--- train_vgg_idiap.py
+++ train_vgg_idiap.py
@@ -35,12 +35,12 @@ def make_lr_batch_size_grid():
 
     max_lr = 1.0
     lr_step = 0.1
-    min_lr_factor = #10
+    min_lr_factor = 1 # 10
     possible_learning_rates = np.asarray([max_lr * lr_step ** (n - 1) for n in range(1, min_lr_factor + 1)])
 
     min_batch_size = 4
     bs_step = 2
-    max_size_factor = 2#8
+    max_size_factor = 2 # 8
     possible_batch_sizes = np.asarray([min_batch_size * bs_step ** (n - 1) for n in range(1, max_size_factor + 1)])
 
     grid = list(itertools.product(possible_learning_rates, possible_batch_sizes))
