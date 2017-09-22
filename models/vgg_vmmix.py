8d3e1195fd29c4dd18c4f6f40125d6775fa1f9a4
sergeyprokudin <sergey.prokudin@gmail.com>
2017-09-22 15:14:11 +0200

    IDIAP dataset training added

--- models/vgg_vmmix.py
+++ models/vgg_vmmix.py
@@ -189,7 +189,7 @@ class BiternionVGGMixture:
         results['log_likelihood_sem'] = float(sem(log_likelihoods, axis=None))
 
         print("MAAD error (%s) : %f ± %fSEM" % (data_part,
-                                                results['maad_mean'],
+                                                results['maad_loss'],
                                                 results['maad_sem']))
         print("log-likelihood (%s) : %f ± %fSEM" % (data_part,
                                                     results['log_likelihood_mean'],
