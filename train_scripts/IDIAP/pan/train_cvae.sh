1235304bafeaeb156a2ad70e392ad25065635bc4
sergeyprokudin <sergey.prokudin@gmail.com>
2017-10-04 11:41:34 +0200

    params tuning

--- train_scripts/IDIAP/pan/train_cvae.sh
+++ train_scripts/IDIAP/pan/train_cvae.sh
@@ -5,7 +5,18 @@ PROJECT_DIR="${SCRIPT_DIR}/../../../"
 PYENV="${PROJECT_DIR}/py_env"
 
 source "${PROJECT_DIR}/scripts/dbash.sh" || exit 1
-dbash::cluster_cuda
+
+CUDNNPATH="/lustre/shared/caffe_shared/cuda_stuff/cudnn-5.1_for_cuda8.0rc"
+CUDAPATH="/lustre/shared/caffe_shared/cuda_stuff/cuda-8.0.27.1_RC"
+
+export PATH=${CUDAPATH}/bin:$PATH
+export CPATH=${CUDAPATH}/include:$CPATH
+export PATH='/lustre/shared/caffe_shared/cuda_stuff/cudnn-5.1_for_cuda8.0rc/bin':$PATH
+export LD_LIBRARY_PATH=${CUDNNPATH}/lib64:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=${CUDAPATH}/lib64:$LD_LIBRARY_PATH
+export LIBRARY_PATH='/lustre/shared/caffe_shared/cuda_stuff/cudnn-5.1_for_cuda8.0rc/lib64':$LIBRARY_PATH
+export LIBRARY_PATH=${CUDAPATH}/lib64:$LIBRARY_PATH
+
 
 source ${PYENV}/bin/activate
 
