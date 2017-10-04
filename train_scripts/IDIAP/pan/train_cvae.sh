473792200173f3d9104932cd4ab447ca774e0c06
sergeyprokudin <sergey.prokudin@gmail.com>
2017-10-04 11:38:11 +0200

    params tuning

--- train_scripts/IDIAP/pan/train_cvae.sh
+++ train_scripts/IDIAP/pan/train_cvae.sh
@@ -4,6 +4,9 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
 PROJECT_DIR="${SCRIPT_DIR}/../../../"
 PYENV="${PROJECT_DIR}/py_env"
 
+source "${PROJECT_DIR}/scripts/dbash.sh" || exit 1
+dbash::cluster_cuda
+
 source ${PYENV}/bin/activate
 
 python "${PROJECT_DIR}/train_cvae.py" "${PROJECT_DIR}/train_configs/IDIAP/pan/train_cvae_idiap_best.yml"
