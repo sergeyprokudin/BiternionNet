7123e3c709d03091213333b15534b19f6910c244
sergeyprokudin <sergey.prokudin@gmail.com>
2017-10-10 14:24:31 +0200

    params tuning

--- train.py
+++ train.py
@@ -23,7 +23,7 @@ def load_config(config_path):
     return config
 
 
-def generate_hyper_params(n_trials):
+def generate_hyper_params(n_trials, model_type):
 
     hyp_params = dict()
     hyp_params['learning_rate'] = ht.sample_exp_float(n_trials, base=10, min_factor=-5, max_factor=-3)
@@ -33,7 +33,12 @@ def generate_hyper_params(n_trials):
     hyp_params['epsilon'] = ht.sample_exp_float(n_trials, base=10, min_factor=-6, max_factor=-8)
     hyp_params['conv_dropout'] = np.random.rand(n_trials)
     hyp_params['fc_dropout'] = np.random.rand(n_trials)
-    hyp_params['n_hidden_units'] = ht.sample_exp_int(n_trials, base=2, min_factor=1, max_factor=4)
+
+    if model_type == 'vm_mixture':
+        hyp_params['n_components'] = ht.sample_exp_int(n_trials, base=2, min_factor=1, max_factor=4)
+    if model_type == 'cvae':
+        hyp_params['n_hidden_units'] = ht.sample_exp_int(n_trials, base=2, min_factor=1, max_factor=4)
+
     hyp_params['vgg_fc_layer_size'] = ht.sample_exp_int(n_trials, base=2, min_factor=5, max_factor=10)
     hyp_params['cvae_fc_layer_size'] = ht.sample_exp_int(n_trials, base=2, min_factor=5, max_factor=10)
 
