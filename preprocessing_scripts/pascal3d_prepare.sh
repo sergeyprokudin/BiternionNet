20e8ce6a135e749fc8d697dc87ce09d1be8f9bb0
Sergey Prokudin <sprokudin@login2.is.localnet>
2018-02-28 22:59:20 +0100

    Cluster notebooks modified

--- preprocessing_scripts/pascal3d_prepare.sh
+++ preprocessing_scripts/pascal3d_prepare.sh
@@ -6,8 +6,12 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
 PYENV="${SCRIPT_DIR}/../py_env"
 source ${PYENV}/bin/activate
 
-source ~/.bashrc
+CUDNNPATH="/lustre/shared/caffe_shared/cuda_stuff/cudnn-5.1_for_cuda8.0rc"
+CUDAPATH="/lustre/shared/caffe_shared/cuda_stuff/cuda-8.0.27.1_RC"
 
-cluster_cuda
+export LD_LIBRARY_PATH=${CUDNNPATH}/lib64:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=${CUDAPATH}/lib64:$LD_LIBRARY_PATH
+
+echo $LD_LIBRARY_PATH
 
 python ../utils/pascal3d.py
