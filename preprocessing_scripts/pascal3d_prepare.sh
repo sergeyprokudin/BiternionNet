b09710d899e4c1fc03fea6edf25728d1ff0de624
Sergey Prokudin <sergey.prokudin@gmail.com>
2018-02-28 22:26:49 +0100

    cluster notebook

--- preprocessing_scripts/pascal3d_prepare.sh
+++ preprocessing_scripts/pascal3d_prepare.sh
@@ -6,4 +6,10 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
 PYENV="${SCRIPT_DIR}/../py_env"
 source ${PYENV}/bin/activate
 
+LPATH="/usr/local/cudnn-5.1/lib64"
+if [[ -d ${LPATH} ]];then
+    dbash::pp "cudnn cluster ${LPATH}"
+    export LD_LIBRARY_PATH=${LPATH}:$LD_LIBRARY_PATH
+fi
+
 python ../utils/pascal3d.py
