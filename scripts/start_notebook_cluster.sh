894f1051354dc0956e45d431b0c27a9b2f584de0
sergeyprokudin <sergey.prokudin@gmail.com>
2018-03-03 20:55:31 +0100

    pascal3d loading fixed

--- scripts/start_notebook_cluster.sh
+++ scripts/start_notebook_cluster.sh
@@ -5,7 +5,8 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
 PYENV="${SCRIPT_DIR}/../py_env"
 source ${PYENV}/bin/activate
 
-CUDNNPATH="/lustre/shared/caffe_shared/cuda_stuff/cudnn-5.1_for_cuda8.0rc"
+#CUDNNPATH="/lustre/shared/caffe_shared/cuda_stuff/cudnn-5.1_for_cuda8.0rc"
+CUDNNPATH="/lustre/shared/caffe_shared/cuda_stuff/cudnnn-6.0_for_cuda8"
 CUDAPATH="/lustre/shared/caffe_shared/cuda_stuff/cuda-8.0.27.1_RC"
 export LD_LIBRARY_PATH=${CUDNNPATH}/lib64:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=${CUDAPATH}/lib64:$LD_LIBRARY_PATH
