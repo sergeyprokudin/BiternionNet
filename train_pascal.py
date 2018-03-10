3bc26790bf1d7eff2f0aa79f4fa94edb0a08de10
Sergey Prokudin <sergey.prokudin@gmail.com>
2018-03-10 22:45:40 +0100

    pascal train script

--- train_pascal.py
+++ train_pascal.py
@@ -130,7 +130,7 @@ def train_model(class_name, loss_type):
 
         train_maad, train_ll, val_maad, val_ll, test_maad, test_ll, kappas = \
             model.train_finetune_eval(x_train, y_train, x_val, y_val, x_test, y_test,
-                                  ckpt_path, batch_size=params['batch_size'], patience=10, epochs=1)
+                                  ckpt_path, batch_size=params['batch_size'], patience=10, epochs=10)
 
     model = BiternionCNN(input_shape=x_train.shape[1:], debug=True, loss_type=loss_type,
                          learning_rate=params['lr'], hlayer_size=params['hlayer_size'])
@@ -140,7 +140,7 @@ def train_model(class_name, loss_type):
 
     train_maad, train_ll, val_maad, val_ll, test_maad, test_ll, kappas = \
         model.train_finetune_eval(x_train, y_train, x_val, y_val, x_test, y_test,
-                                  ckpt_path, batch_size=params['batch_size'], patience=10, epochs=1)
+                                  ckpt_path, batch_size=params['batch_size'], patience=10, epochs=200)
 
     with open(global_results_log, 'a') as f:
         res_str = '%s;%2.2f;%2.2f;%2.2f;%2.2f;%2.2f;%2.2f;%2.2f;%2.2f;%2.2f'\
