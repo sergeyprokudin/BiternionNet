f2e4ea1a6feef78ec6a8e13aee153d50ee348d66
Sergey Prokudin <sergey.prokudin@gmail.com>
2018-03-12 00:38:33 +0100

    mixture model done

--- train_pascal_mixture.py
+++ train_pascal_mixture.py
@@ -129,12 +129,12 @@ def train_model(class_name):
     ckpt_path = os.path.join(LOGS_PATH, ckpt_name)
 
     K.clear_session()
-    model = BiternionMixture(input_shape=x_train.shape[1:], debug=True,
-                         learning_rate=params['lr'], hlayer_size=params['hlayer_size'])
+    model = BiternionMixture(input_shape=x_train.shape[1:], debug=False,
+                             learning_rate=params['lr'], hlayer_size=params['hlayer_size'])
 
     train_maad, train_ll, val_maad, val_ll, test_maad, test_ll = \
         model.train_finetune_eval(x_train, y_train, x_val, y_val, x_test, y_test,
-                                  ckpt_path, batch_size=params['batch_size'], patience=10, epochs=1)
+                                  ckpt_path, batch_size=params['batch_size'], patience=10, epochs=100)
 
     with open(global_results_log, 'a') as f:
         res_str = '%s;%2.2f;%2.2f;%2.2f;%2.2f;%2.2f;%2.2f;%2.2f;%2.2f;%2.2f'\
