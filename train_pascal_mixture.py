3208b8043a88d516e85b194b6e06cae0b9395ec1
Sergey Prokudin <sergey.prokudin@gmail.com>
2018-03-12 01:13:20 +0100

    mixture model done

--- train_pascal_mixture.py
+++ train_pascal_mixture.py
@@ -134,7 +134,7 @@ def train_model(class_name):
 
     train_maad, train_ll, val_maad, val_ll, test_maad, test_ll = \
         model.train_finetune_eval(x_train, y_train, x_val, y_val, x_test, y_test,
-                                  ckpt_path, batch_size=params['batch_size'], patience=10, epochs=100)
+                                  ckpt_path, batch_size=params['batch_size'], patience=20, epochs=100)
 
     with open(global_results_log, 'a') as f:
         res_str = '%s;%2.2f;%2.2f;%2.2f;%2.2f;%2.2f;%2.2f;%2.2f;%2.2f;%2.2f'\
